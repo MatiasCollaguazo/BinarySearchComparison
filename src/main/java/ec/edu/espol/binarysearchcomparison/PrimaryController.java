94f70a7fbabc53088a687beba8f2530027b1fbb2
MatiasCollaguazo <mcollagu@espol.edu.ec>
2024-06-22 11:29:59 -0500

    Initial files

---
+++ src/main/java/ec/edu/espol/binarysearchcomparison/PrimaryController.java
@@ -0,0 +1,21 @@
+package ec.edu.espol.binarysearchcomparison;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class PrimaryController implements Initializable{
+
+    @FXML
+    private Button primaryButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+    
+    
+    
+}
---
+++ src/main/java/ec/edu/espol/binarysearchcomparison/modelo/BinarySearchAlgorithm.java
@@ -0,0 +1,9 @@
+package ec.edu.espol.binarysearchcomparison.modelo;
+
+/**
+ *
+ * @author Group #13
+ */
+public class BinarySearchAlgorithm {
+    
+}
