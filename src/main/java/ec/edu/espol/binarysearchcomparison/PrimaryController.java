5fe33fbb9e2b9afc7b22932085b9eaeac85bbf01
MatiasCollaguazo <mcollagu@espol.edu.ec>
2024-06-22 21:48:20 -0500

    Added minor utilities

--- src/main/java/ec/edu/espol/binarysearchcomparison/PrimaryController.java
+++ src/main/java/ec/edu/espol/binarysearchcomparison/PrimaryController.java
@@ -4,15 +4,32 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 
-public class PrimaryController implements Initializable{
+public class PrimaryController implements Initializable {
 
+    @FXML
+    private Text nElements;
+    @FXML
+    private Slider nSlider;
+    @FXML
+    private Text txtLinearSearch;
+    @FXML
+    private Text txtBS_Iterative;
+    @FXML
+    private Text txtBS_Recursive;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
+        nSlider.setValue(0);
+        nElements.setText("10");
     }
-    
-    
-    
+
+    @FXML
+    private void updateNElements(MouseEvent event) {
+        int nValue = (int) Math.floor(nSlider.getValue());
+        nElements.setText(String.valueOf(nValue));
+    }
 }
