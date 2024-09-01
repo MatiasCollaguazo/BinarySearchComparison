07ce7d5b0cb71956c2c1723741f559a1cffaf802
MatiasCollaguazo <mcollagu@espol.edu.ec>
2024-09-01 11:32:38 -0500

    Minor changes

--- src/main/java/ec/edu/espol/binarysearchcomparison/modelo/BinarySearchAlgorithmRecursive.java
+++ src/main/java/ec/edu/espol/binarysearchcomparison/modelo/BinarySearchAlgorithmRecursive.java
@@ -21,24 +21,29 @@ public class BinarySearchAlgorithmRecursive implements SearchAlgorithm {
      * @return El índice del elemento si se encuentra, de lo contrario -1.
      */
     private int binarySearchRecursive(int[] array, int target, int left, int right) {
+        // Verificar si los límites son válidos para continuar la búsqueda
         if (left <= right) {
+            // Calcular el índice medio de la sección actual del array
             int mid = (right + left) / 2;
 
-            // Check if the target is at the mid index
+            // Comprobar si el elemento objetivo se encuentra en el índice medio
             if (array[mid] == target) {
-                return mid;
+                return mid; // Elemento encontrado, devolver el índice
             }
 
-            // If the target is smaller than the mid element, search in the left subarray
-            if (array[mid] > target) {
+            // Si el objetivo es menor que el valor en el índice medio
+            if (target < array[mid]) {
+                // Buscar en la submatriz izquierda (elementos menores al medio)
                 return binarySearchRecursive(array, target, left, mid - 1);
+            } else {
+                // Si el objetivo es mayor que el valor en el índice medio
+                // Buscar en la submatriz derecha (elementos mayores al medio)
+                return binarySearchRecursive(array, target, mid + 1, right);
             }
-
-            // If the target is larger than the mid element, search in the right subarray
-            return binarySearchRecursive(array, target, mid + 1, right);
         }
 
-        // Target not found
+        // Si no se encuentra el objetivo, devolver -1
         return -1;
     }
+
 }
