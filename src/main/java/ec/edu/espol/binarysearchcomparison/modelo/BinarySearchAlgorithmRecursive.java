0cf37021bf33f542c636043380631573469cdda6
MatiasCollaguazo <mcollagu@espol.edu.ec>
2024-09-01 11:08:48 -0500

    Update BinarySearchAlgorithmRecursive.java

--- src/main/java/ec/edu/espol/binarysearchcomparison/modelo/BinarySearchAlgorithmRecursive.java
+++ src/main/java/ec/edu/espol/binarysearchcomparison/modelo/BinarySearchAlgorithmRecursive.java
@@ -21,24 +21,21 @@ public class BinarySearchAlgorithmRecursive implements SearchAlgorithm {
      * @return El índice del elemento si se encuentra, de lo contrario -1.
      */
     private int binarySearchRecursive(int[] array, int target, int left, int right) {
-        if (left <= right) {
-            int mid = (right + left) / 2;
-
-            // Check if the target is at the mid index
-            if (array[mid] == target) {
-                return mid;
-            }
-
-            // If the target is smaller than the mid element, search in the left subarray
-            if (array[mid] > target) {
-                return binarySearchRecursive(array, target, left, mid - 1);
-            }
-
-            // If the target is larger than the mid element, search in the right subarray
-            return binarySearchRecursive(array, target, mid + 1, right);
+        if (left > right) {
+            return -1; // No encontrado
         }
 
-        // Target not found
-        return -1;
+          int mid = (int) Math.floor((left+right)/2); // Calcular el índice medio
+
+        if (array[mid] == target) { // Comparar con el valor en el índice medio
+            return mid; // Devolver el índice
+        }
+
+        if (target < array[mid]) { // Si el objetivo es menor que el valor en el medio
+            right = mid - 1; // Buscar en la mitad izquierda del array, actualizando el límite superior
+        } else { // Si no, buscar en la mitad derecha del array
+            left = mid + 1; // Actualizando el límite inferior
+        }
+        return binarySearchRecursive(array, target, left, right);
     }
 }
